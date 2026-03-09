797c1def46e95188099915b7af5f91667cf4162b
Evgenii Chechelev <echechelev87@gmail.com>
2026-03-09 11:39:23 +0300

    Add options.set_capability

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,8 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture(scope='function')
 def setup_browser():
     options = Options()
+    options.set_capability("browserName", "chrome")
+    options.set_capability("browserVersion", "127.0")
 
     driver = webdriver.Remote(
         command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
@@ -13,5 +15,5 @@ def setup_browser():
     )
 
     yield driver
-
+    driver.quit()
 
